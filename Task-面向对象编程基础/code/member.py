0a521273330ffd5c91bb74eaefc18e93540b6f87
Nannan-yu <2200876915@qq.com>
2026-05-30 00:34:59 +0800

    Update member.py

--- Task-面向对象编程基础/code/member.py
+++ Task-面向对象编程基础/code/member.py
@@ -1,1 +1,9 @@
+class Member:
+  _id=1 #_开头变成私有属性（类属性）
+  def __init__(self,name,group):
+    self.name=name
+    self.group=group
+    self.points=0
+    self.id=f"RM{Member._id:04d}"
+    _id+=1
 
