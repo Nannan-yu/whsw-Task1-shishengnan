e5c21d29e30a9f59f7c242b7331c31e67815261f
Nannan-yu <2200876915@qq.com>
2026-05-30 21:02:25 +0800

    Update member_manager.py

--- Task-面向对象编程基础/code/member_manager.py
+++ Task-面向对象编程基础/code/member_manager.py
@@ -1,3 +1,11 @@
 class MemberManager:
-  group=["视觉","电控","机械","运营"]
-  def __init__(self)
+  groups=["视觉","电控","机械","运营"]
+  def __init__(self):
+    self.members=[]
+  #判断组别是否正确
+  def _judgegroup(self,group):
+    return group in self.groups
+  #判断姓名是否重复
+  def _judgename(self,name):
+    for i in self.members:
+      if 
