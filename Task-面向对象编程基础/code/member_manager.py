fc27561147163b7c48f9b928494c2f20cead4dba
Nannan-yu <2200876915@qq.com>
2026-05-30 21:48:25 +0800

    Update member_manager.py

--- Task-面向对象编程基础/code/member_manager.py
+++ Task-面向对象编程基础/code/member_manager.py
@@ -1,11 +1,24 @@
+class member import 
 class MemberManager:
   groups=["视觉","电控","机械","运营"]
   def __init__(self):
     self.members=[]
-  #判断组别是否正确
+  #判断组别是否正确,正确True
   def _judgegroup(self,group):
     return group in self.groups
-  #判断姓名是否重复
+  #判断姓名是否重复，重复True
   def _judgename(self,name):
     for i in self.members:
-      if 
+      if i.members ==name:
+        return True
+    return False
+  #增加成员
+def add_member(self,name,group):
+  if not _judgegroup(group):#if后面为True则执行
+    print("×，没有这个组")
+    return False
+  if _jugdename(name):
+    print(f"×，队员{name}已存在")
+    return False
+  member=
+  
