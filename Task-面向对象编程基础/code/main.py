54c0be7c623173e8b1c54997836830378a957c0f
Nannan-yu <2200876915@qq.com>
2026-05-30 23:41:25 +0800

    Update main.py

--- Task-面向对象编程基础/code/main.py
+++ Task-面向对象编程基础/code/main.py
@@ -13,3 +13,24 @@ def add_member(mgr):
   name = input("请输入队员姓名：").strip()
   group = input("请输入组别（视觉/电控/机械/运营）：").strip()
   mgr.add_member(name, group)
+def list_member(mgr):
+  print("\n--- 所有队员 ---")
+  mgr.all_member()
+def add_memberpoints(mgr):
+  print("\n--- 为队员加分 ---")
+  member_id = input("请输入队员编号：").strip()
+  points = int(input("请输入要加的分数："))
+  mgr.add_points(member_id, points)
+def cut_memberpoints(mgr):
+  print("\n--- 为队员扣分 ---")
+  member_id = input("请输入队员编号：").strip()
+  points = int(input("请输入要扣的分数："))
+  mgr.cut_points(member_id, points)
+def rank_members(mgr):
+  print("\n--- 积分排行榜 ---")
+  mgr.show_rankings()
+def delete_members(mgr):
+  print("\n--- 删除队员 ---")
+  member_id = input("请输入要删除的队员编号：").strip()
+  mgr.delete_member(member_id)
+
